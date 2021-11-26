1a53f21e4426f4668113052f664fa5c87a49db86
구소현 <kusohyeon@gmail.com>
2021-11-27 01:08:26 +0900

    스프링 시큐리티: 아키텍처 - Authentication

---
+++ src/main/java/com/security/inflearnspringsecurity/form/SampleService.java
@@ -0,0 +1,26 @@
+package com.security.inflearnspringsecurity.form;
+
+import com.security.inflearnspringsecurity.account.Account;
+import com.security.inflearnspringsecurity.account.AccountContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class SampleService {
+    public void dashboard() {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Object principal = authentication.getPrincipal();
+//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//        boolean authenticated = authentication.isAuthenticated();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("=========================");
+        System.out.println(userDetails.getUsername());
+    }
+}
